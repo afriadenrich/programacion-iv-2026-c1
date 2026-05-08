7683c0ebb406d493ef37c8bd78fdec7dd78cc361
AgustinFrich <agustinfrich@gmail.com>
2026-05-07 21:29:49 -0300

    interceptors

--- clase-08-interceptors/src/app/app.ts
+++ clase-08-interceptors/src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('clase-08-interceptors');
+  API_URL = 'https://api.nasa.gov/planetary/apod';
+
+  http = inject(HttpClient);
+
+  fecha: string = '2026-05-07';
+
+  resultado = signal<string>('');
+  texto = signal<string>('');
+  async buscar() {
+    const peticion = this.http.get(this.API_URL, {
+      params: {
+        date: this.fecha,
+      },
+    });
+
+    peticion.subscribe((data: any) => {
+      this.resultado.set(data.url);
+      this.texto.set(data.title);
+    });
+  }
 }
